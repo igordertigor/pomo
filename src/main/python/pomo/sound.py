938b71b20abbfc49df318fee68d492c70b67277a
Ingo F <ingo.fruend@twentybn.com>
2017-07-20 14:46:48 -0400

    fixing processing

--- src/main/python/pomo/sound.py
+++ src/main/python/pomo/sound.py
@@ -6,7 +6,8 @@ import subprocess
 class SoundPlayer(object):
 
     def __init__(self, sound):
-        if not subprocess.run(['which', 'mpg321']):
+        if not subprocess.run(['which', 'mpg321'],
+                              stderr=subprocess.PIPE).returncode:
             self.cmd = ['mpg321', sound]
         else:
             self.cmd = None
@@ -15,7 +16,7 @@ class SoundPlayer(object):
 
     def play(self):
         if self.cmd is not None:
-            subprocess.run(self.cmd)
+            subprocess.run(self.cmd, stderr=subprocess.PIPE)
         else:
             music.load(self.sound)
             music.play()
